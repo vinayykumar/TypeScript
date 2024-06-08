1e71754d83760081336145afc27f8dff18fe96ce
Vinay Kumar <Vinayy1709@gmail.com>
2024-06-09 00:39:23 +0530

    Generics in function Declaration/Calling

--- app.ts
+++ app.ts
@@ -91,6 +91,8 @@ const bankAccount:BankAccount = {
 
 
 Generics
+
+EXAMPLE 1
 function logString(str:string) : string {
     console.log(str);
     return str;
@@ -106,7 +108,89 @@ function logArray(arr:any[]){
     return arr;
 }
 
+using T as a placeholder
 function logAnything<T>(arg:T):T{
     console.log(arg);
     return arg;
 }
+
+
+EXAMPLE 2
+interface HasAge{
+    age : number
+}
+
+function getOldest(people:HasAge[]):HasAge{
+    return people.sort((a,b) => b.age - a.age)[0];
+}
+
+Using Generics as placeholder 
+function getOldest<T extends HasAge>(people:T[]):T{
+    return people.sort((a,b) => b.age - a.age)[0];
+}
+
+const people : HasAge[] = [
+    { age : 40},
+    { age : 30},
+    { age : 20},
+    { age : 55}
+];
+
+interface Player {
+    name : string;
+    age : number;
+}
+
+const players : Player[]  = [
+    { name : 'John Doe' , age : 30},
+    { name : 'Kevin 11' , age : 20},
+    { name : 'Vinay Kumar' , age : 22},
+]
+
+const person = getOldest(players) as Player;
+const person = getOldest(players);
+const person = getOldest(people);
+console.log(getOldest(players));
+
+
+EXAMPLE 3
+
+interface IPost{
+    title : string;
+    id : number;
+    description : string
+}
+
+interface IUser{
+    id : number;
+    name : string;
+    age : number;
+}
+
+const fetchPostData = async (path:string): Promise<IPost[]> => {
+    const response = await  fetch(`http://example.com${path}`);
+    return response.json();
+}
+
+const fetchUserData = async (path:string): Promise<IUser[]> => {
+    const response = await  fetch(`http://example.com${path}`);
+    return response.json();
+}
+
+const fetchData = async <ResultType>(path:string):Promise<ResultType> => {
+    const response = await  fetch(`http://example.com${path}`);
+    return response.json();
+}  
+
+( async () => {
+    const users = await fetchData<IUser[]>('/users');
+})
+
+( async () => {
+    const posts = await fetchPostData('/posts');
+})
+
+( async () => {
+    const users = await fetchUserData('/users');
+    users[0].name;
+})
