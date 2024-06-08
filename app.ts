b09ed3b3f97ef04ec733724709b176b450abb9ee
Vinay Kumar <Vinayy1709@gmail.com>
2024-06-08 14:46:25 +0530

    typetest 1

---
+++ app.ts
@@ -0,0 +1,15 @@
+
+//to run directly
+// tsc-node file_name.ts
+
+// to ru by JS
+// 1.tsc file_name
+// 2.node file_name
+
+let num:number = 1;
+
+function calc(num1:number,num2:number){
+    return num1 + num2;
+}
+
+console.log(calc(20,30))
